7c01224edea1d32412f636d0892c5ea4766b48f7
Hasan <eljabirhasan@gmail.com>
2023-10-12 15:19:54 +0700

    Add black transparent color asset

--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -2,7 +2,8 @@ const colors = {
 	colorPrimary: "#2196F3",
 	error: "#EF5350",
 	borderInput: "#BDBDBD",
-	disabled: "#9E9E9E"
+	disabled: "#9E9E9E",
+	blackTransparent: "#00000060"
 }
 
 export default colors;
